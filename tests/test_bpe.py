50e633b697cf81724b53eb0dbd5df1708672ea6a
chaewonkong <chaewonkong@gmail.com>
2026-09-10 23:01:14 +0900

    refactor: use class to store merge table

--- tests/test_bpe.py
+++ tests/test_bpe.py
@@ -1,16 +1,19 @@
-from llm_scratch.bpe.py.bpe import merge, most_common_pairs, encode, decode
+from llm_scratch.bpe.py.bpe import BPE
 
 
 def test_most_common_pairs():
+    bpe = BPE()
     ids = [1,1,2,3,4,2,3,1,2,1,2]
-    assert most_common_pairs(ids) == (1,2)
+    assert bpe.most_common_pairs(ids) == (1,2)
 
 def test_merge():
+    bpe = BPE()
     ids = [1,2,1,3,1,2]
-    assert merge(ids, (1,2), 99) == [99, 1, 3, 99]
+    assert bpe.merge(ids, (1,2), 99) == [99, 1, 3, 99]
 
 def test_round_trip():
     text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
 
-    ids, merges = encode(text)
-    assert decode(ids, merges) == text
+    bpe = BPE()
+    bpe.train(text, 256)
+    assert bpe.decode(bpe.encode(text)) == text
