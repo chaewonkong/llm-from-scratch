b8d13073d207e1de1e512dc3cc24d10a704866cc
chaewonkong <chaewonkong@gmail.com>
2026-09-13 22:46:42 +0900

    test: add more cases

--- tests/test_regex_tokenizer.py
+++ tests/test_regex_tokenizer.py
@@ -14,3 +14,46 @@ def test_roundtrip():
 
     # then
     assert text == round_trip_result
+
+
+def test_roundtrip_korean():
+    # given
+    text = "대규모 언어 모델(LLM)은 방대한 양의 데이터를 학습하여 자연어 및 기타 유형의 콘텐츠를 이해하고 생성하여 광범위한 작업을 수행할 수 있는 딥 러닝의 카테고리입니다. LLM은 단어 시퀀스를 처리하고 텍스트의 패턴을 포착하는 데 탁월한 신경망 아키텍처의 일종(트랜스포머라고 함)을 기반으로 구축됩니다."
+    tokenizer = RegexTokenizer()
+
+    tokenizer.train(text=text, vocab_size=100256)
+
+    # when
+    round_trip_result = tokenizer.decode(tokenizer.encode(text))
+
+
+    # then
+    assert text == round_trip_result
+
+def test_roundtrip_emoji():
+    # given
+    text = "😀😀🍕🍺🍕🍺🍕🍺😂😂😂🐶🐱🐶🐱❤️❤️🍕🍺🎉🎉🎉😀🐶🐱🍕🍺❤️😂😂😀😀🍕🍺🍕🍺🍕🍺😂😂😂🐶🐱🐶🐱❤️❤️🍕🍺🎉🎉🎉😀🐶🐱🍕🍺❤️😂😂👍🏽👍🏽👨‍👩‍👧👨‍👩‍👧🇰🇷🇰🇷🇰🇷👍🏽🍕🍕👨‍👩‍👧🇰🇷"
+    tokenizer = RegexTokenizer()
+
+    tokenizer.train(text=text, vocab_size=100256)
+
+    # when
+    round_trip_result = tokenizer.decode(tokenizer.encode(text))
+
+
+    # then
+    assert text == round_trip_result
+
+def test_roundtrip_empty():
+    # given
+    text = "                                                                                                                                                       "
+    tokenizer = RegexTokenizer()
+
+    tokenizer.train(text=text, vocab_size=100256)
+
+    # when
+    round_trip_result = tokenizer.decode(tokenizer.encode(text))
+
+
+    # then
+    assert text == round_trip_result
